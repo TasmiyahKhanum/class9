954dc927366f12871b2a1cf10d7ea73caa042bf4
TasmiyahKhanum <92870691+TasmiyahKhanum@users.noreply.github.com>
2021-10-20 19:49:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,50,50);
+}
+
+function draw() 
+{
+  background("green");
+  if(keyDown("RIGHT_ARROW")){
+  box.x=box.x+3;}
+  if(keyDown("LEFT_ARROW")){
+    box.x=box.x-3;
+  }
+    if(keyDown("UP_ARROW")){
+      box.y=box.y-3;
+  }
+  if(keyDown("DOWN_ARROW")){
+    box.y=box.y+3;
+}
+ drawSprites();
+ 
+}
+
+
+
+
